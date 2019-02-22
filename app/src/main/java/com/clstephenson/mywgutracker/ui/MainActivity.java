263b4b31037e5258924cd34898bb51db6793190b
Chris Stephenson <clstephenson@gmail.com>
2019-02-22 13:00:17 -0700

    Remove options menu from main activity.

--- app/src/main/java/com/clstephenson/mywgutracker/ui/MainActivity.java
+++ app/src/main/java/com/clstephenson/mywgutracker/ui/MainActivity.java
@@ -1,7 +1,6 @@
 package com.clstephenson.mywgutracker.ui;
 
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -47,25 +46,6 @@ public class MainActivity extends AppCompatActivity
         fragmentManager = getSupportFragmentManager();
 
         openHomeFragment();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
